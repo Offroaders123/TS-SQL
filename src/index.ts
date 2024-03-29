ae2fbc9cd722f2b407b9e9ac8cf26ae84472836a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-29 00:51:25 -0700

    Accessing the Database, Setup
    https://www.youtube.com/watch?v=Hej48pi_lOc
    https://github.com/Sam-Meech-Ward/Node-MySQL
    https://philna.sh/blog/2023/09/05/nodejs-supports-dotenv/
    https://stackoverflow.com/questions/8940230/how-to-run-sql-script-in-mysql
    
    ```sh
    npx tsx --env-file ./.env --watch ./src/index.ts
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import * as mysql from "mysql2";
+import { env } from "node:process";
+import { createPool } from "mysql2";
+
+const host: string = env["MYSQL_HOST"]!;
+const user: string = env["MYSQL_USER"]!;
+const password: string = env["MYSQL_PASSWORD"]!;
+const database: string = env["MYSQL_DATABASE"]!;
+
+const pool = createPool({
+  host,
+  user,
+  password,
+  database
+}).promise();
+
+const result = await pool.query("SELECT * FROM notes");
+console.log(result);
