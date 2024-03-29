22a679ac0c2d5914542f1ef4541a8b824dc1349a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-29 00:30:15 -0700

    Database Schema
    Found the repo for the demo video I'm using to follow along with figuring out the MySQL stack, for a Node project and just in the general sense as well.
    
    https://www.youtube.com/watch?v=Hej48pi_lOc
    https://github.com/Sam-Meech-Ward/Node-MySQL

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE notes_app;
+USE notes_app;
+
+CREATE TABLE notes (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  contents TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO notes (title, contents)
+VALUES
+("The starting one", "Some example text"),
+("Another two", "Watched the documentary videos about Empath today!");
